bc4c30c379e9dd1b058545e826264ca8f8c84a39
Aaron Parecki <aaron@parecki.com>
2015-11-17 16:16:30 -0800

    add encrypted config files

--- config.template.php
+++ config.template.php
@@ -2,7 +2,10 @@
 class Config {
 
   public static $baseURL = 'http://localhost:8080';
-  public static $authServerURL = 'https://example.com/oauth/authorize';
-  public static $secretKey = 'xxx';
+  public static $limitRequestsPerMinute = 6;
 
+  public static $authServerURL = 'https://example.com/oauth2/authorize';
+  public static $tokenEndpoint = 'https://example.com/oauth2/token';
+
+  public static $secretKey = 'xxx';
 }
