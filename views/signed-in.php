02d881e372d6312e476bd65275420c0d783d8bc9
Aaron Parecki <aaron@parecki.com>
2018-12-21 13:47:24 -0800

    remove code from URL

--- views/signed-in.php
+++ views/signed-in.php
@@ -1,3 +1,7 @@
 <?php $this->layout('layout', ['title' => $title]); ?>
 
 <p>You successfully signed in! Now return to your device to finish.</p>
+
+<script>
+window.history.replaceState({}, false, '/auth/redirect');
+</script>
